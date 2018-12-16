eba51c150e24a5fad4b0bd9b5d52cda7ac8bfeee
SebJercz <43208797+SebJercz@users.noreply.github.com>
2018-12-16 00:16:49 -0500

    fig

--- pince/graph.py
+++ pince/graph.py
@@ -5,6 +5,8 @@ import numpy as np
 
 x = [1.5, 2.0, 3.6, 4.1]
 y = [0.17, 0.37, 1.3, 2.0]
+x2 = [1.5, 4.1]
+y2 = [0.16, 1.56]
 
 # SKIIIIIIIA . boum
 majorLocator1 = MultipleLocator(1)                       #gros intervalle axe x
@@ -15,8 +17,8 @@ majorFormatter2 = FormatStrFormatter('%.1f')
 minorLocator2 = MultipleLocator(0.25)                       #petit intervalle axe y
 
 
-#reflexion = plt.plot(x, y, 'k--', marker="s", label = 'Résolution en y')
-transmission = plt.plot(x, y, 'k', marker="o", markersize=6, markeredgewidth=1, markeredgecolor='k', markerfacecolor='k', label = 'Résolution en x')
+reflexion = plt.plot(x2, y2, 'r--', label = 'Tendance linéaire')
+transmission = plt.plot(x, y, 'k', marker="o", markersize=6, markeredgewidth=1, markeredgecolor='k', markerfacecolor='k', label = 'Résultats expérimentaux')
 
 plt.gca().xaxis.set_major_locator(majorLocator1)
 plt.gca().tick_params(axis='both', labelsize = 14)    #tick size
@@ -31,8 +33,8 @@ plt.xlabel(r'Puissance du laser infrarouge [W]', fontsize=14)                   
 plt.ylabel(r'Constante de trappe $k_t$   [$\mu$N/m]', fontsize=14)                              #nom axe y, font size
 plt.rc('legend', fontsize=12)    # legend fontsize
 
-#plt.legend(bbox_to_anchor=(0.4, 0.94), loc=2, borderaxespad=0., frameon=False)               #pos legende
-plt.errorbar(x, y, xerr=[0.01, 0.01, 0.02, 0.03], yerr=[0.01, 0.05, 0.2, 0.4], fmt='ko', markersize=3, ecolor='k', capsize=6,)
+plt.legend(bbox_to_anchor=(0.4, 0.94), loc=2, borderaxespad=0., frameon=False)               #pos legende
+plt.errorbar(x, y, xerr=[0.01, 0.01, 0.02, 0.03], yerr=[0.01, 0.07, 0.2, 0.45], fmt='ko', markersize=3, ecolor='k', capsize=6,)
 #plt.errorbar(x, y, xerr=0.5, yerr=2, fmt='wo', markersize=4, ecolor='k', capsize=2,)
 #plt.axhline(y=edge1, color='r', linestyle='-')
 #plt.axhline(y=edge9, color='r', linestyle='-')
